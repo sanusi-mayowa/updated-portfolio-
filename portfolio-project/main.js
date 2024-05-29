5b6ce007735513d7a9d2586342de0bc59a09f14b
sanusi-mayowa <sanusifaruq007@gmail.com>
2024-05-29 16:41:56 +0100

    update

---
+++ portfolio-project/main.js
@@ -0,0 +1,49 @@
+
+    // =================================
+    const menu = document.querySelector(".nav_menu");
+    const menuBtn = document.querySelector("#open-menu-btn");
+    const closeBtn = document.querySelector("#close-menu-btn");
+
+
+
+
+
+    menuBtn.addEventListener('click', () => {
+        menu.style.display = "flex";
+        closeBtn.style.display = "inline-block";
+        menuBtn.style.display = "none"
+    })
+
+
+
+
+
+
+
+
+
+    // close==============
+    const closeNav = () => {
+        menu.style.display = "none";
+        closeBtn.style.display = "none";
+        menuBtn.style.display = "inline-block";
+    }
+
+    closeBtn.addEventListener('click', closeNav)
+
+
+
+
+
+
+
+
+
+
+
+    //  change navbar styles on scroll
+
+window.addEventListener('scroll',()=>{
+    document.querySelector('nav').classList.toggle
+    ('window-scroll',window.scrollY > 0)
+} )
